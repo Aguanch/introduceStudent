3b91f0c81e604ba55834f728e851b4d9e364d4c4
Aaron Guanch <aguan007@fiu.edu>
2023-02-02 11:05:26 -0500

    Initial Commit

---
+++ introduceStudent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  introduceStudent
+//
+//  Created by D G on 2/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
